1ba0c28cbef2bf7e407afe1c131b89fd243d378f
senmore <xiaxia.self@gmail.com>
2023-07-26 10:09:59 +0800

    chore: Use the concurrently package to start the client and backend server (#13)
    Co-authored-by: jiaxiaxia <jiaxiaxia@jiaxiaxias-MacBook-Pro.local>

--- projects/client/vite.config.js
+++ projects/client/vite.config.js
@@ -2,6 +2,8 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
+import { readFileSync } from 'fs';
+
 const env = loadEnv('development', process.cwd());
 export default defineConfig({
   plugins: [
@@ -16,10 +18,15 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0',
+    https: {
+      key: readFileSync('../server/server.key', 'utf8'),
+      cert: readFileSync('../server/server.cert', 'utf8'),
+    },
     proxy: {
       '/api': {
-        target: env.VITE_SERVER_HOST,
+        target: env.VITE_SERVER_HOST || 'https://0.0.0.0:6030/',
         changeOrigin: true,
+        secure: false, // 忽略自签名证书错误
       },
     },
   },
